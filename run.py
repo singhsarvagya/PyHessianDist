144cc6c088cc72b7dffb17aed14773a21fae09e9
Sarvagya Singh <sarvagya_singh@berkeley.edu>
2020-04-22 11:10:32 -0700

    cleaned up run.py

--- run.py
+++ run.py
@@ -26,13 +26,7 @@ from density_plot import get_esd_plot
 from models.resnet import resnet
 
 from pyhessian.utils import group_product, group_add, normalization, get_params_grad, hessian_vector_product, orthnormal
-# from pyhessian import hessian
-# from multiprocessing import set_start_method
-# try:
-#     set_start_method('spawn')
-# except RuntimeError:
-#     pass
-
+from typing import List, Callable
 
 
 #*
@@ -76,6 +70,10 @@ parser.set_defaults(eigenvalue=False)
 parser.set_defaults(trace=False)
 parser.set_defaults(density=False)
 
+# for parallel computing
+parser.add_argument('--ip', type=str, required=True, help='ip address of the machine for distributed computing')
+parser.add_argument('--device_count', type=int, required=True, help='number of available devices')
+
 args = parser.parse_args()
 
 # set random seed to reproduce the work
@@ -86,9 +84,6 @@ if args.cuda:
 for arg in vars(args):
     print(arg, getattr(args, arg))
 
-# total available devices
-device_count = 2
-
 # # get dataset
 train_loader, test_loader = getData(name='cifar10_without_data_augmentation',
                                     train_bs=args.mini_hessian_batch_size,
@@ -98,8 +93,7 @@ train_loader, test_loader = getData(name='cifar10_without_data_augmentation',
 ##############
 assert (args.hessian_batch_size % args.mini_hessian_batch_size == 0)
 batch_num = args.hessian_batch_size // args.mini_hessian_batch_size
-#
-# # getting the dataset batches
+# getting the dataset batches
 hessian_dataloader = []
 for i, (inputs, labels) in enumerate(train_loader):
     hessian_dataloader.append((inputs, labels))
@@ -107,11 +101,10 @@ for i, (inputs, labels) in enumerate(train_loader):
         break
 
 # dividing dataset into partitions
-print(len(hessian_dataloader))
-assert len(hessian_dataloader) % device_count == 0
+assert len(hessian_dataloader) % args.device_count == 0
 # partitioning data into number of GPUs available
-data_partitions = DataPartitioner(hessian_dataloader, [len(hessian_dataloader) // device_count] * device_count)
-
+data_partitions = DataPartitioner(hessian_dataloader,
+                                  [len(hessian_dataloader) // args.device_count] * args.device_count)
 
 # get model
 model = resnet(num_classes=10,
@@ -119,51 +112,50 @@ model = resnet(num_classes=10,
                residual_not=args.residual,
                batch_norm_not=args.batch_norm)
 
-
-criterion = nn.CrossEntropyLoss()  # label loss
+# label loss
+criterion = nn.CrossEntropyLoss()
 
 ###################
 # Get model checkpoint, get saving folder
 ###################
 if args.resume == '':
     raise Exception("please choose the trained model")
-
+# loading the state dictionary into the model
 state_dict = torch.load(args.resume, map_location=torch.device('cpu'))
+# since model was trained using DataParallel, have to remove 'module.'
+# from state dictionary keys
 state_dict_ = OrderedDict()
 for key in state_dict.keys():
     new_key = key[7:]
     state_dict_[new_key] = state_dict[key]
 model.load_state_dict(state_dict_)
 
-# test(model, test_loader, cuda=False)
 
-"""Non-blocking point-to-point communication."""
+def hv_product(rank: int, size: int, model: nn.Module, data: List[torch.Tensor], v: List[torch.Tensor]):
+    # group of processes
+    group = dist.new_group(list(range(size)))
+    # current process device
+    device = torch.device("cuda:{}".format(rank))
 
-
-def dataloader_hv_product(v, model, data, device, size):
-    num_data = 0  # count the number of datum points in the dataloader
-
+    # count the number of datum points in the dataloader
+    num_data = 0
+    # to accumulate the hessian vector product
     THv = []
 
     for inputs, targets in data:
-        # print(inputs.shape)
-        # print(inputs[0, 0, 0, 7])
         model.zero_grad()
         tmp_num_data = inputs.size(0)
         outputs = model(inputs.to(device))
         loss = criterion(outputs, targets.to(device))
-        # print(loss)
         loss.backward(create_graph=True)
         params, gradsH = get_params_grad(model)
         model.zero_grad()
-        # print(gradsH[4][13])
         Hv = torch.autograd.grad(gradsH,
                                  params,
                                  grad_outputs=v,
                                  only_inputs=True,
                                  retain_graph=False)
-        # print(Hv[4][13])
-        # exit(0)
+        # accumulating the eigenvector
         if len(THv) == 0:
             THv = [
                 Hv1.contiguous() * float(tmp_num_data) + 0.
@@ -176,29 +168,21 @@ def dataloader_hv_product(v, model, data, device, size):
             ]
         num_data += float(tmp_num_data)
 
-    # TODO reduce Hv
-    group = dist.new_group(range(size))
+    # reducing the THv after every i
     for t in range(len(THv)):
         dist.all_reduce(THv[t], op=dist.ReduceOp.SUM, group=group)
-    # print (THv[0][0, 0])
-    # exit(0)
+
     THv = [THv1 / float(size * num_data) for THv1 in THv]
     eigenvalue = group_product(THv, v).cpu().item()
     return eigenvalue, THv
 
 
-""" All-Reduce example."""
-def run(rank, size, model, data):
-    """ Simple point-to-point communication. """
-    group = dist.new_group(range(size))
+def eigenvalue(rank: int, size: int, model: nn.Module, data: List[torch.Tensor],
+        max_iter: int = 100, tol: float = 1e-3, top_n: int = 1):
+    # group of processes
+    group = dist.new_group(list(range(size)))
+    # current process device
     device = torch.device("cuda:{}".format(rank))
-    # tensor = torch.ones(1).to(device)
-    # dist.all_reduce(tensor, op=dist.ReduceOp.SUM, group=group)
-    # print('Rank ', rank, ' has data ', tensor[0])
-
-    maxIter = 20
-    tol = 1e-3
-    top_n = 1
 
     assert top_n >= 1
 
@@ -207,31 +191,30 @@ def run(rank, size, model, data):
 
     computed_dim = 0
 
+    # moving model to respective GPU
     model = model.to(device)
-    params, gradsH = get_params_grad(model)
+    # setting model to eval mode
     model.eval()
-
-    if rank == 0:
-        start = time.time()
+    # getting model params and gradients
+    params, gradsH = get_params_grad(model)
 
     while computed_dim < top_n:
         eigenvalue = None
-        v = [torch.randn(p.size()).to(device) for p in params]  # generate random vector
+        # generate random vector
+        v = [torch.randn(p.size()).to(device) for p in params]
         v = normalization(v)
 
+        # since we have manually set the seed for cuda
+        # same v should be initialized across all GPUs
+        # but this step is necessary if the seed is
+        # not set
         for t in v:
             dist.broadcast(t, src=0, group=group)
 
-        for i in range(maxIter):
-            # print (i)
+        for i in range(max_iter):
             v = orthnormal(v, eigenvectors)
-            model.zero_grad()
 
-            tmp_eigenvalue, Hv = dataloader_hv_product(v, model, data, device, size)
-            # print (tmp_eigenvalue)
-            # print (Hv[0][0, 0, 0])
-            # exit(0)
-
+            tmp_eigenvalue, Hv = hv_product(rank, size, model, data, v)
             v = normalization(Hv)
 
             if eigenvalue is None:
@@ -243,34 +226,38 @@ def run(rank, size, model, data):
                 else:
                     eigenvalue = tmp_eigenvalue
 
+            # TODO remove this later
             if rank == 0:
-                print (eigenvalue)
+                print(eigenvalue)
 
         eigenvalues.append(eigenvalue)
         eigenvectors.append(v)
         computed_dim += 1
 
+    # TODO remove this later
     if rank == 0:
         print(eigenvalues)
-        end = time.time()
-        print("Time to compute top eigenvalue: %f" % (end - start))
 
 
-def init_process(rank, size, model, data, fn, backend='nccl'):
+def init_process(rank: int, size: int, model: nn.Module, data: List[torch.Tensor],
+                 fn: Callable, ip: str, backend: str = 'nccl'):
     """ Initialize the distributed environment. """
-    os.environ['MASTER_ADDR'] = '128.32.35.224'
-    os.environ['MASTER_PORT'] = '29512'
+    os.environ['MASTER_ADDR'] = ip
+    os.environ['MASTER_PORT'] = '29512'  # random port for now
     dist.init_process_group(backend, rank=rank, world_size=size)
     fn(rank, size, model, data)
 
 
 if __name__ == "__main__":
-    size = device_count
     processes = []
-    for rank in range(size):
-        p = Process(target=init_process, args=(rank, size, model, data_partitions.use(rank), run))
+    start = time.time()
+    for rank in range(args.device_count):
+        p = Process(target=init_process, args=(rank, args.device_count, model, data_partitions.use(rank),
+                                               eigenvalue, args.ip))
         p.start()
         processes.append(p)
 
     for p in processes:
         p.join()
+    end = time.time()
+    print("Time to compute top eigenvalue: %f" % (end - start))
